a4d5f39982e27802780c55ec329e24ebd4be2698
Nos <zvezdazanatih@gmail.com>
2025-09-01 18:17:25 +0700

    0

---
+++ go-app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func helloWorld(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello from Co in docker ")
+}
---
+++ python-app/main.py
@@ -0,0 +1,9 @@
+import calendar
+print('Добро пожаловать в СУПЕР календарь\n')
+
+year =int(input('введите год:'))
+month =int(input('введите номер месяца:'))
+
+print(calendar.month(year, month))
+
+print('Всего!!!')
